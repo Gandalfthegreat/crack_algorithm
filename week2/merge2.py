71371d5012215da82a68298b356ea4b0c682f91a
wangchao11 <wangchao11@kuaishou.com>
2021-07-07 21:39:04 +0800

    feat: summary

---
+++ week2/merge2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if not lists:
+            return
+        n = len(lists)
+        return self.merge(lists, 0, n - 1)
+
+    def merge(self, lists, left, right):
+        if left == right:
+            return lists[left]
+        mid = left + (right - left) // 2
+        l1 = self.merge(lists, left, mid)
+        l2 = self.merge(lists, mid + 1, right)
+        return self.merge2Lists(l1, l2)
+
+    def merge2Lists(self, l1, l2):
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val <= l2.val:
+            l1.next = self.merge2Lists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.merge2Lists(l1, l2.next)
+            return l2
